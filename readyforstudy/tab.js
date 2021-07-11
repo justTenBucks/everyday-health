c49d9116520c2b749cddba35c6ee5cb6c04666ee
justTenBucks <jojo6103@hanmail.net>
2021-07-11 18:18:43 +0900

    make it better

--- readyforstudy/tab.js
+++
@@ -1,8 +0,0 @@
-
-for (let i = 0; i < 3; i++) {
-    $(".tab-button").eq(i).click(function(){
-        $(".tab-button").removeClass('active');
-        $(".tab-content").removeClass('show');
-        $(".tab-button").eq(i).addClass('active');
-        $(".tab-content").eq(i).addClass('show');
-})}
